73fe7d9d351e4c1f920c9b8ce929905c2cab6c64
Sungki <youbyme@gmail.com>
2023-02-02 22:35:48 -0500

    Added more pygame codes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-# import pygame module in this program
 import pygame as pg
+import moderngl as mgl
+import struct
+import sys
 from os import environ
 from sys import platform as _sys_platform
 import math
@@ -12,45 +14,77 @@ def platform():
     elif _sys_platform in ('win32', 'cygwin'):
         return 'win'
 
-class Cardioid:
-    def __init__(self, app):
-        self.app = app
-        self.radius = 400
-        self.num_lines = 200
-        self.translate = self.app.screen.get_width() // 2, self.app.screen.get_height() // 2
-        self.counter, self.inc = 0, 0.01
-
-    def draw(self):
-        time = pg.time.get_ticks()
-        self.radius = 350 + 50 * abs(math.sin(time * 0.004) - 0.5)
-        factor = 1 + 0.0001 * time
-
-        for i in range(self.num_lines):
-            theta = (2 * math.pi / self.num_lines) * i
-            x1 = int(self.radius * math.cos(theta)) + self.translate[0]
-            y1 = int(self.radius * math.sin(theta)) + self.translate[1]
-
-            x2 = int(self.radius * math.cos(factor * theta)) + self.translate[0]
-            y2 = int(self.radius * math.sin(factor * theta)) + self.translate[1]
-
-            pg.draw.aaline(self.app.screen, (255,255,255), (x1, y1), (x2, y2))
-
 class App:
-    def __init__(self):
-        self.screen = pg.display.set_mode([1600, 900])
-        self.clock = pg.time.Clock()
-        self.cardioid = Cardioid(self)
+    def __init__(self, win_size=(1920, 1080)):
+        # opengl context
+        pg.display.set_mode(win_size, flags=pg.OPENGL | pg.DOUBLEBUF | pg.FULLSCREEN)
+        self.ctx = mgl.create_context()
 
-    def draw(self):
-        self.screen.fill((0,0,0))
-        self.cardioid.draw()
+        # time objects
+        self.clock = pg.time.Clock()
+
+        self.path = ""
+
+        if platform()=="android":
+            self.path="/data/data/org.test.pgame/files/app/"
+        elif platform()=="linux":
+            self.path="./"
+
+        # load shaders
+        with open(self.path+'programs/vertex.glsl') as f:
+            vertex = f.read()
+        with open(self.path+'programs/fragment.glsl') as f:
+            fragment = f.read()
+        self.program = self.ctx.program(vertex_shader=vertex, fragment_shader=fragment)
+
+        # quad screen vertices
+        vertices = [(-1, -1), (1, -1), (1, 1), (-1, 1), (-1, -1), (1, 1)]
+
+        # quad vbo
+        vertex_data = struct.pack(f'{len(vertices) * len(vertices[0])}f', *sum(vertices, ()))
+        self.vbo = self.ctx.buffer(vertex_data)
+
+        # quad vao
+        self.vao = self.ctx.vertex_array(self.program, [(self.vbo, '2f', 'in_position')])
+
+        # uniforms
+        self.set_uniform('u_resolution', win_size)
+
+    def render(self):
+        self.ctx.clear()
+        self.vao.render()
         pg.display.flip()
+
+    def update(self):
+        self.set_uniform('u_time', pg.time.get_ticks() * 0.001)
 
     def run(self):
         while True:
-            self.draw()
-            [exit() for i in pg.event.get() if i.type == pg.QUIT]
-            self.clock.tick(60)
+            self.check_events()
+            self.update()
+            self.render()
+            self.clock.tick(0)
+            fps = self.clock.get_fps()
+            pg.display.set_caption(f'{fps :.1f}')
+
+    def set_uniform(self, u_name, u_value):
+        try:
+            self.program[u_name] = u_value
+        except KeyError:
+            pass
+
+    def destroy(self):
+        self.vbo.release()
+        self.program.release()
+        self.vao.release()
+
+    def check_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                self.destroy()
+                pg.quit()
+                sys.exit()
+
 
 if __name__ == '__main__':
     app = App()
